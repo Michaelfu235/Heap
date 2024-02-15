2fa3aad6a0641fa07a441d895ee928ad187e6511
U-BSD\453117 <453117@SHS-9YBF6X3.beaverton.k12.or.us>
2024-02-15 14:36:05 -0800

    finished add function and files

--- main.cpp
+++ main.cpp
@@ -10,12 +10,39 @@ using namespace std;
 
 
 void printTree(int* heap, int index, int depth);
+void add(int* &heap, int data, int index);
+
 int main(){
   int* heap = new int[100];
   char inpt[15];
-
+  int indexon = 1;
+  /*
   for(int i = 0;i<100;i++){
     heap[i] = i;
+    }*/
+
+  while(true){
+    cout << "what file name you wat" << endl;
+    int inputt = 0;
+    char* fileName = new char[80];
+    cin >> fileName;
+    cin.ignore();
+    fstream file;
+    file.open(fileName);
+    while(file >> inputt){
+      add(heap, inputt, indexon);
+      indexon++;
+    }
+    file.close();
+    
+    //cin >> inputt;
+    //cin.ignore();
+    //add(heap, inputt, indexon);
+    //indexon++;
+
+    cout << "-----------------------------------------------------------------" << endl;
+    printTree(heap, 1, 0);
+    cout << "-----------------------------------------------------------------" << endl;
   }
 
   printTree(heap, 1, 0);
@@ -48,5 +75,20 @@ void printTree(int* heap, int index, int depth){
   if(index*2 <= 100 && heap[index*2] != 0){
     
     printTree(heap, index*2, depth+1);
+  }
+}
+
+
+void add(int* &heap, int data, int index){
+  if(index>1){
+    heap[index] = data;
+    int parentData = heap[index/2];
+    if(data > parentData){
+      heap[index/2] = data;
+      heap[index] = parentData;
+      add(heap, data, index/2);
+    }
+  } else {
+    heap[index] = data;
   }
 }
