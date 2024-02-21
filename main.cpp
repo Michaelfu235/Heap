5109f6a515d5eebd83925d678b261e54c04dfbe8
U-BSD\453117 <453117@SHS-9YBF6X3.beaverton.k12.or.us>
2024-02-21 14:17:45 -0800

    everything works now

--- main.cpp
+++ main.cpp
@@ -11,18 +11,80 @@ using namespace std;
 
 void printTree(int* heap, int index, int depth);
 void add(int* &heap, int data, int index);
+void deleteRoot(int* &heap, int index);
+
 
 int main(){
   int* heap = new int[100];
   char inpt[15];
   int indexon = 1;
+  bool justKeepGoing = true;
   /*
   for(int i = 0;i<100;i++){
     heap[i] = i;
     }*/
 
-  while(true){
-    cout << "what file name you wat" << endl;
+  while(justKeepGoing == true){
+    cout << "-------------------------------------------------------------------------" << endl;
+    cout << "What do you want to do? Add(A), Delete(D), Print(P), and Quit(Q)" << endl;
+    char* inputtt = new char[20];
+    cin >> inputtt;
+    cin.ignore();
+    int num = 0;
+    if(strcmp(inputtt, "A")==0){
+      cout << "Manually(M) or from File(F)" << endl;
+      cin >> inputtt;
+      cin.ignore();
+      if(strcmp(inputtt, "M")==0){
+	
+	cout << "what number to enter?" << endl;
+	cin >> num;
+	cin.ignore();
+	int size = 1;
+	while(heap[size] != 0){
+	  size++;
+	}
+	add(heap, num, size);
+      } else if(strcmp(inputtt, "F")==0){
+	cout << "what file name? (include .txt)" << endl;
+	cin >> inputtt;
+	cin.ignore();
+	fstream file;
+	file.open(inputtt);
+	while(file >> num){
+	  int size = 1;
+	  while(heap[size] != 0){
+	    size++;
+	  }
+	  add(heap, num, size);
+	}
+	file.close();
+      }
+    } else if (strcmp(inputtt, "D")==0){
+      cout << "just delete the largest element(L) or everything(E)" << endl;
+      cin >> inputtt;
+      cin.ignore();
+      if(strcmp(inputtt, "L")==0){
+	deleteRoot(heap, 1);
+      } else if (strcmp(inputtt, "E")==0){
+        while(heap[1] != 0){
+	  deleteRoot(heap, 1);
+	}
+
+      }
+    
+    } else if (strcmp(inputtt, "P")==0){
+      printTree(heap, 1, 0);
+    } else if (strcmp(inputtt, "Q")==0){
+      justKeepGoing = false;
+    }
+
+    
+    
+  }
+    
+
+  /*
     int inputt = 0;
     char* fileName = new char[80];
     cin >> fileName;
@@ -40,19 +102,28 @@ int main(){
     //add(heap, inputt, indexon);
     //indexon++;
 
+    deleteRoot(heap, 1);
+    
     cout << "-----------------------------------------------------------------" << endl;
     printTree(heap, 1, 0);
     cout << "-----------------------------------------------------------------" << endl;
   }
+  
 
-  printTree(heap, 1, 0);
+  printTree(heap, 1, 0);*/
 }
 
 
 
 
 void printTree(int* heap, int index, int depth){
+  if(heap[1]==0){
+    cout << "empty database" << endl;
+    return;
+  }
+
   //Go through the right
+  
   if(index*2+1 <= 100 && heap[index*2+1] != 0){
     
     printTree(heap, index*2+1, depth+1);
@@ -91,4 +162,35 @@ void add(int* &heap, int data, int index){
   } else {
     heap[index] = data;
   }
+}
+
+
+void deleteRoot(int* &heap, int index){
+  int size = 1;
+  while(heap[size] != 0){
+    size++;
+  }
+
+  size = size-1;
+
+  cout << "value: " << heap[1] << endl;
+  heap[1] = heap[size];
+  heap[size] = 0;
+
+  int tempy = 1;
+  while(heap[tempy] < heap[tempy * 2] || heap[tempy] < heap[tempy*2+1]){
+    if(heap[tempy*2] > heap[tempy*2+1]){
+      int temp = heap[tempy];
+      heap[tempy] = heap[tempy*2];
+      heap[tempy*2] = temp;
+      tempy = tempy*2;
+    } else if(heap[tempy*2] < heap[tempy*2+1]){
+      int temp = heap[tempy];
+      heap[tempy] = heap[tempy*2+1];
+      heap[tempy*2+1] = temp;
+      tempy = tempy*2;
+    }
+    
+  }
+  
 }
